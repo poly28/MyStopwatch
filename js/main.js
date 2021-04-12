d0890b67b9282b96593ed3a074c6f3354690e389
poly28 <horimasa0328@gmail.com>
2021-04-13 06:59:09 +0900

    タイマー機能完成

--- js/main.js
+++ js/main.js
@@ -6,18 +6,52 @@ const stop = document.getElementById('stop');
 const reset = document.getElementById('reset');
 
 let startTime;
-
-// console.log(Date.now());
+let timeoutId;
+let elapsedTime = 0;
 
 function countUp() {
-	console.log(Date.now() - startTime);
-
-	setTimeout(() => {
+	const d = new Date(Date.now() - startTime + elapsedTime);
+	const m = String(d.getMinutes()).padStart(2, '0');
+	const s = String(d.getSeconds()).padStart(2, '0');
+	const ms = String(d.getMilliseconds()).padStart(3, '0');
+	timer.textContent = `${m}:${s}.${ms}`;
+	timeoutId = setTimeout(() => {
 		countUp();
 	}, 10);
 }
 
+function setButtonStateInitial() {
+	start.disabled = false;
+	stop.disabled = true;
+	reset.disabled = true;
+}
+function setButtonStateRunning() {
+	start.disabled = true;
+	stop.disabled = false;
+	reset.disabled = true;
+}
+function setButtonStateStopped() {
+	start.disabled = false;
+	stop.disabled = true;
+	reset.disabled = false;
+}
+
+setButtonStateInitial();
+
 start.addEventListener('click', () => {
+	setButtonStateRunning();
 	startTime = Date.now();
 	countUp();
+});
+
+stop.addEventListener('click', () => {
+	setButtonStateStopped();
+	clearTimeout(timeoutId);
+	elapsedTime += Date.now() - startTime;
+});
+
+reset.addEventListener('click', () => {
+	setButtonStateInitial();
+	timer.textContent = '00:00.000';
+	elapsedTime = 0;
 });
