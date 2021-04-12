d99af44cadadcc5b03b169655178c96e186cf4f1
poly28 <horimasa0328@gmail.com>
2021-04-12 22:14:38 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const timer = document.getElementById('timer');
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+
+let startTime;
+
+// console.log(Date.now());
+
+function countUp() {
+	console.log(Date.now() - startTime);
+
+	setTimeout(() => {
+		countUp();
+	}, 10);
+}
+
+start.addEventListener('click', () => {
+	startTime = Date.now();
+	countUp();
+});
